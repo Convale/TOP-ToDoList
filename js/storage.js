d22cc0a7a6195aa1e615dff4a34cec7fd8955d68
Shaun Payne <convale@gmail.com>
2020-06-18 18:49:37 -0700

    moved some functions to storage.js

--- js/storage.js
+++ js/storage.js
@@ -16,6 +16,37 @@ function updateTaskStorage(projectNum, taskNum, taskType, taskObject) {
     localStorage.projects = JSON.stringify(projectsLS);
 }
 
+function storageAvailable(type) {
+    //function source: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+
+        if (localStorage.getItem("projects") === null) {
+            loadOrigData();
+        }
+
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
 function loadOrigData() {
     //Test Tasks
     const taskNum1 = new Task("Workout", new Date(2020, 11, 31), 1, 120, "Doing great with your routine, keep it up!", "open");
